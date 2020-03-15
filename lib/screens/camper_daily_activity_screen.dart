3af85a29d012b481e91ddb7356cb09b8ed3633e4
Melike Bilen <bilenmelike@gmail.com>
2020-03-15 14:36:46 +0300

    dropdownmenu added, cant choose yet

--- lib/screens/camper_daily_activity_screen.dart
+++ lib/screens/camper_daily_activity_screen.dart
@@ -16,28 +16,83 @@ class CamperDailyActivityScreen extends StatefulWidget {
 class _CamperDailyActivityScreenState extends State<CamperDailyActivityScreen> {
   @override
   Widget build(BuildContext context) {
-
     Camper chosenCamper = ModalRoute.of(context).settings.arguments;
     chosenCamper.dailyActivitiesOfCamper = [null, null, null];
 
-    
+// DropdownButton getButton(String minor, int a, List<Activity> actList){
+//     return  DropdownButton<Activity>(
+//             hint: Text(minor),
+//             onChanged: (value) {
+//               setState(() {
+//                 chosenCamper.dailyActivitiesOfCamper[a] = value;
+//               });
+//             },
+//             if(a==0){
+//                 items: Provider.of<Activities>(context).activities.map((act) {
+//               return DropdownMenuItem<Activity>(
+//                 value: act,
+//                 child: Text(act.name),
+//               );
+//             }).toList();
+//             }
+          
+//           );
+// }
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('${chosenCamper.name}\'s activities for today!'),
       ),
-      body: DropdownButton<Activity>(
-        hint: Text('Minor 1'),
-        //value: chosenCamper.dailyActivitiesOfCamper[0],
-        onChanged: (Activity chosen) {
-          setState(() {
-           chosenCamper.dailyActivitiesOfCamper[0] = chosen;
-          });
-        },
-        items: Provider.of<Activities>(context).activities1.map((act) {
-          return DropdownMenuItem<Activity>(
-            child: Text(act.name),
-          );
-        }).toList(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          DropdownButton<Activity>(
+            hint: Text('Minor 1'),
+            onChanged: (value) {
+              setState(() {
+                chosenCamper.dailyActivitiesOfCamper[0] = value;
+              });
+            },
+            items: Provider.of<Activities>(context).activities1.map((act) {
+              return DropdownMenuItem<Activity>(
+                value: act,
+                child: Text(act.name),
+              );
+            }).toList(),
+          ),
+          SizedBox(height: 10,),
+          DropdownButton<Activity>(
+            hint: Text('Minor 2'),
+            onChanged: (value) {
+              setState(() {
+                chosenCamper.dailyActivitiesOfCamper[1] = value;
+              });
+            },
+            items: Provider.of<Activities>(context).activities2.map((act) {
+              return DropdownMenuItem<Activity>(
+                value: act,
+                child: Text(act.name),
+              );
+            }).toList(),
+          ),
+          SizedBox(height: 10,),
+           DropdownButton<Activity>(
+            hint: Text('Minor 3'),
+            onChanged: (value) {
+              setState(() {
+                chosenCamper.dailyActivitiesOfCamper[2] = value;
+              });
+            },
+            items: Provider.of<Activities>(context).activities3.map((act) {
+              return DropdownMenuItem<Activity>(
+                value: act,
+                child: Text(act.name),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
